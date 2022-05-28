bbe3d9bd16a25e7fb74c406c2b59c5751d59d7ec
aizhan2111ads <104277257+aizhan2111ads@users.noreply.github.com>
2022-05-28 11:40:45 +0600

    Create ThisVertex.java

---
+++ ThisVertex.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ThisVertex<T> {
+    private T data;
+    private ThisVertex<T>;
+  
+    }
+public int arrList() {
+        return arrList;
+    }
+}
